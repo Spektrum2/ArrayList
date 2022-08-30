de31a56de79a5d41b291973a6139ab5d59261ae7
admin <promitay@gmail.com>
2022-08-30 18:38:00 +0300

    Домашняя работа 2.14 Основы алгоритмов, часть 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringList stringList = new ArrayStringList();
+        stringList.add("Hello");
+        stringList.add("HelloTwo");
+        StringList stringList2 = new ArrayStringList();
+        stringList2.add("Hello");
+        stringList2.add("HelloTwo");
+        System.out.println(stringList.equals(stringList2));
+        }
     }
-}
---
+++ src/exceptions/ValueNullException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class ValueNullException extends RuntimeException{
+    public ValueNullException(String message) {
+        super(message);
+    }
+}
